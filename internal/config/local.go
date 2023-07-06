f04e285a485dc57dbff2610d69f599b8b8e1af58
Keloran <keloran@develbox.info>
2023-07-06 21:56:13 +0100

    http was on wrong port

--- internal/config/local.go
+++ internal/config/local.go
@@ -9,7 +9,7 @@ type Local struct {
 	KeepLocal   bool `env:"BUGFIXES_LOCAL_ONLY" envDefault:"false" json:"keep_local,omitempty"`
 	Development bool `env:"DEVELOPMENT" envDefault:"false" json:"development,omitempty"`
 	GRPCPort    int  `env:"GRPC_PORT" envDefault:"3000" json:"grpc_port,omitempty"`
-	HTTPPort    int  `env:"HTTP_PORT" envDefault:"3001" json:"http_port,omitempty"`
+	HTTPPort    int  `env:"HTTP_PORT" envDefault:"80" json:"http_port,omitempty"`
 }
 
 func BuildLocal(cfg *Config) error {
